2dd3f3bc7431c459d760e91f440e6f8b5da89268
Wiki <otti@petitcode.com>
2019-05-27 14:33:47 +0200

    Uploaded as-graphics&#x2F;index.php

---
+++ uploads/as-graphics/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html class="ng-csp" data-placeholder-focus="false" lang="en" data-locale="en" >
+	<head
+ data-requesttoken="IQI+Ot2oMGC9nsci5fXzU6zTgQzbuCdGaDgO2KuOmww=:bnENTZDKVQbl2YRH1p7COOT4rki4i3R+Anpl4d258E0=">
+		<meta charset="utf-8">
+		<title>
+		Nextcloud		</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+		<meta name="apple-itunes-app" content="app-id=1125420102">
+		<meta name="theme-color" content="#0082c9">
+		<link rel="icon" href="/core/img/favicon.ico">
+		<link rel="apple-touch-icon-precomposed" href="/core/img/favicon-touch.png">
+		<link rel="mask-icon" sizes="any" href="/core/img/favicon-mask.svg" color="#0082c9">
+		<link rel="manifest" href="/index.php/apps/theming/manifest?v=0">
+		<link rel="stylesheet" href="/core/vendor/select2/select2.css?v=cc5c1f6b-0">
+<link rel="stylesheet" href="/core/vendor/jquery-ui/themes/base/jquery-ui.css?v=cc5c1f6b-0">
+<link rel="stylesheet" href="/apps/deck/css/activity.css?v=0959eaaf-0">
+<link rel="stylesheet" href="/apps/files_versions/css/versions.css?v=4fda350b-0">
+<link rel="stylesheet" href="/apps/files_pdfviewer/css/style.css?v=1bf6e69c-0">
+<link rel="stylesheet" href="/apps/files_videoplayer/css/style.css?v=1bf6e69c-0">
+<link rel="stylesheet" href="/core/css/guest.css?v=cc5c1f6b-0">
+		<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/index.php/core/js/oc.js?v=cc5c1f6b"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/vendor/core.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/index.php/js/core/merged-template-prepend.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/search/js/search.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/js/backgroundjobs.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/index.php/js/core/merged-share-backend.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/apps/firstrunwizard/js/jquery.colorbox.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/apps/firstrunwizard/js/firstrunwizard.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/apps/files_pdfviewer/js/previewplugin.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/apps/files_videoplayer/js/viewer.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/search/js/searchprovider.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/js/jquery-ui-fixes.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/js/files/fileinfo.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/js/files/client.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/js/contactsmenu.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/js/contactsmenu_templates.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/core/vendor/jsTimezoneDetect/jstz.js?v=cc5c1f6b-0"></script>
+<script nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0=" defer src="/index.php/js/core/merged-login.js?v=cc5c1f6b-0"></script>
+		<link rel="stylesheet" href="/index.php/apps/theming/styles?v=0"/><script defer src="/index.php/apps/theming/js/theming?v=0" nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0="></script><script defer src="/index.php/apps/accessibility/js/accessibility?v=0" nonce="SVFJK090Mm9NR0M5bnNjaTVmWHpVNnpUZ1F6YnVDZEdhRGdPMkt1T213dz06Ym5FTlRaREtWUWJsMllSSDFwN0NPT1Q0cmtpNGkzUitBbnBsNGQyNThFMD0="></script><meta property="og:title" content="Nextcloud"/><meta property="og:description" content="a safe home for all your data"/><meta property="og:site_name" content="Nextcloud"/><meta property="og:url" content="https://nextcloud.petitcode.com/"/><meta property="og:type" content="website"/><meta property="og:image" content="https://nextcloud.petitcode.com/core/img/favicon-touch.png"/>	</head>
+	<body id="body-login">
+		<noscript>
+	<div id="nojavascript">
+		<div>
+			This application requires JavaScript for correct operation. Please <a href="https://www.enable-javascript.com/" target="_blank" rel="noreferrer noopener">enable JavaScript</a> and reload the page.		</div>
+	</div>
+</noscript>
+		<div class="wrapper">
+			<div class="v-align">
+									<header role="banner">
+						<div id="header">
+							<div class="logo">
+								<h1 class="hidden-visually">
+									Nextcloud								</h1>
+															</div>
+						</div>
+					</header>
+								<main>
+					
+<!--[if IE 8]><style>input[type="checkbox"]{padding:0;}</style><![endif]-->
+<form method="post" name="login">
+	<fieldset>
+									<div id="message" class="hidden">
+			<img class="float-spinner" alt=""
+				src="/core/img/loading-dark.gif">
+			<span id="messageText"></span>
+			<!-- the following div ensures that the spinner is always inside the #message div -->
+			<div style="clear: both;"></div>
+		</div>
+		<p class="grouptop">
+			<input type="text" name="user" id="user"
+				placeholder="Username or email"
+				aria-label="Username or email"
+				value=""
+				autofocus				autocomplete="on" autocapitalize="none" autocorrect="off" required>
+			<label for="user" class="infield">Username or email</label>
+		</p>
+
+		<p class="groupbottom">
+			<input type="password" name="password" id="password" value=""
+				placeholder="Password"
+				aria-label="Password"
+								autocomplete="on" autocapitalize="none" autocorrect="off" required>
+			<label for="password" class="infield">Password</label>
+		</p>
+
+		<div id="submit-wrapper">
+			<input type="submit" id="submit" class="login primary" title="" value="Log in" disabled="disabled" />
+			<div class="submit-icon icon-confirm-white"></div>
+		</div>
+
+		
+		
+				<div id="reset-password-wrapper" style="display: none;">
+			<input type="submit" id="reset-password-submit" class="login primary" title="" value="Reset password" disabled="disabled" />
+			<div class="submit-icon icon-confirm-white"></div>
+		</div>
+		
+		<div class="login-additional">
+						<div class="lost-password-container">
+				<a id="lost-password" href="">
+					Forgot password?				</a>
+				<a id="lost-password-back" href="" style="display:none;">
+					Back to login				</a>
+			</div>
+					</div>
+
+		<input type="hidden" name="timezone_offset" id="timezone_offset"/>
+		<input type="hidden" name="timezone" id="timezone"/>
+		<input type="hidden" name="requesttoken" value="IQI+Ot2oMGC9nsci5fXzU6zTgQzbuCdGaDgO2KuOmww=:bnENTZDKVQbl2YRH1p7COOT4rki4i3R+Anpl4d258E0=">
+	</fieldset>
+</form>
+				</main>
+			</div>
+		</div>
+		<footer role="contentinfo">
+			<p class="info">
+				<a href="https://nextcloud.com" target="_blank" rel="noreferrer noopener" class="entity-name">Nextcloud</a> – a safe home for all your data			</p>
+		</footer>
+	</body>
+</html>
